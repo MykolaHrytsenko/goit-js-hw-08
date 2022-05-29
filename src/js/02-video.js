b48e6db550627b9c71ec728c1ad9a71c3a210f87
MykolaHrytsenko <niksandler@gmail.com>
2022-05-29 18:21:16 +0300

    task 3.2 and 3.3 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,3 @@
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
-
-// const onPlay = function (seconds) {
-//     localStorage.setItem('videoplayer-current-time', seconds)
-// }
-
-// player.on('timeupdate');
-
-// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-// player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function (duration) {
-//     // seconds = the actual time that the player seeked to
-// }).catch(function (error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
-
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
-
-// import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,25 +1,42 @@
+import throttle from 'lodash.throttle';
+
 const refs = {
     form: document.querySelector('.feedback-form'),
     input: document.querySelector('[name="email"]'),
     message: document.querySelector('[name="message"]')
 }
-
 const LOCAL_KEY = "feedback-form-state";
+refs.form.addEventListener('input', throttle(inputData, 500));
 
-// console.log(refs.input);
-// console.log(refs.message);
-
-const handleSubmit = (evt) => {
-    evt.preventDefault();
-
+function inputData() {
     const localData = {
         email: refs.input.value,
         message: refs.message.value
     }
-    console.log(localData);
-
     localStorage.setItem(LOCAL_KEY, JSON.stringify(localData));
+};
+
+const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if (refs.message.value === "" || refs.input.value === "") {
+        alert(`Заповніть всі поля, будьласка!`);
+        return;
+    }
     refs.input.value = "";
-}
+    refs.message.value = "";
+
+    console.log(localStorage.getItem(LOCAL_KEY));
+};
 
 refs.form.addEventListener('submit', handleSubmit);
+
+function localCheckData() {
+    const savedData = JSON.parse(localStorage.getItem(LOCAL_KEY));
+    // console.log(savedData);
+    if (savedData === null) {
+        return
+    }
+    refs.input.value = savedData.email;
+    refs.message.value = savedData.message;
+};
+localCheckData();
