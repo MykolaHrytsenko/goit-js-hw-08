76a5a60356b59edca63e2ab273af7ca4509931c0
MykolaHrytsenko <niksandler@gmail.com>
2022-05-29 17:11:00 +0300

    add 3.1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('[name="email"]'),
+    message: document.querySelector('[name="message"]')
+}
 
+const LOCAL_KEY = "feedback-form-state";
+
+// console.log(refs.input);
+// console.log(refs.message);
+
+const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    const localData = {
+        email: refs.input.value,
+        message: refs.message.value
+    }
+    console.log(localData);
+
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(localData));
+    refs.input.value = "";
+}
+
+refs.form.addEventListener('submit', handleSubmit);
